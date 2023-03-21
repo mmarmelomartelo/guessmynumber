9db2709d2da07aea6a373025ad2daabd4321c9cc
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-21 13:39:23 +0000

    adds js conditional logic to check if the secret number is correct and displays it to the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-console.log("Hi we are connected!");
-
-const secret_number = Math.trunc(Math.random() * 30) + 1;
-document.querySelector(".number").textContent = secret_number;
-
-console.log(document.querySelector(".message").textContent = " 🎉Correct Number!");
+const secreNumber = Math.trunc(Math.random() * 30) + 1;
+document.querySelector(".number").textContent = secreNumber;
 
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
 
     if (!guess) {
         document.querySelector(".message").textContent = "🖐 No a number!";
+
+    } else if (guess === secreNumber) {
+        document.querySelector(".message").textContent = " 🎉Correct Number!";
     }
 
 
