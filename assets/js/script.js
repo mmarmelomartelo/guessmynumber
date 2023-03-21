5281d3197ec74511aa01e5f9f2edfe41238e94fc
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-21 11:54:32 +0000

    adds js code to select the butotn check! and d add an event listener upon a mouse click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 console.log("Hi we are connected!");
+
+console.log(document.querySelector(".message").textContent = " 🎉Correct Number!");
+document.querySelector(".check").addEventListener("click", function() {
+    console.log(document.querySelector(".guess").value);
+})
