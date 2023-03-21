d28d52345d84c48bbe7abff7e2b2e8e58ec05424
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-21 20:14:20 +0000

    adds js code to hide the secret number from the user and shows if the guessing is correct.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,31 @@
-const secreNumber = Math.trunc(Math.random() * 30) + 1;
-document.querySelector(".number").textContent = secreNumber;
+const secretNumber = Math.trunc(Math.random() * 30) + 1;
 let score = 30;
+
+
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
 
     if (!guess) {
         document.querySelector(".message").textContent = "🖐 No a number!";
 
-    } else if (guess === secreNumber) {
+    } else if (guess === secretNumber) {
         document.querySelector(".message").textContent = " 🎉Correct Number!";
-
+        document.querySelector(".number").textContent = secretNumber;
         document.querySelector("body").style.backgroundColor = "#60b347";
-    } else if (guess > secreNumber) {
+        document.querySelector('.number').style.width = '30rem';
+
+    } else if (guess > secretNumber) {
         document.querySelector(".message").textContent = " 📈Too high!";
         score--;
         document.querySelector(".score").textContent = score;
-    } else if (guess < secreNumber) {
+    } else if (guess < secretNumber) {
         document.querySelector(".message").textContent = " 📉Too low!";
         score--;
         document.querySelector(".score").textContent = score;
     }
 
     if (score <= 0) {
-        document.querySelector(".message").textContent = "😣 Game over!"
+        document.querySelector(".message").textContent = "😣 Game Over!"
         document.querySelector("body").style.backgroundColor = "#a84c32";
 
     }
